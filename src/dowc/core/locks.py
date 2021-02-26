5edb41cdce43e68a5ec41270f44beaeea63466bb
Daniel Ammeraal <danielammeraal@gmail.com>
2021-02-26 14:40:11 +0100

    Removed unused locking of files by server

--- src/dowc/core/locks.py
+++ src/dowc/core/locks.py
@@ -1,31 +1,21 @@
-from django.core import signing
-
+import uuid
 from djangodav.base.locks import BaseLock
 
 
 class WebDAVLock(BaseLock):
+    """
+    This feature is unused in the current implementation as files
+    are protected by django url routing and the serializer.
+    """
     def acquire(self, lockscope, locktype, depth, timeout, owner):
+        """
+        Returns random uuid to satisfy WebDAV client.
+        """
         # TODO: ACTUALLY LOCK THE FILE
-        # For now creating a token out of given arguments and secret key.
-        token = signing.dumps(
-            {
-                "lockscope": lockscope,
-                "locktype": locktype,
-                "depth": depth,
-                "timeout": timeout,
-                "owner": owner,
-            }
-        )
-        return token
+        return uuid.uuid4()
 
     def release(self, token):
-        # TODO: ACTUALLY RELEASE THE FILE
-        data_dict = signing.loads(token)
-
-        # For now adding some pseudo checks for testing purposes.
-        req_dict_fields = ["lockscope", "locktype", "depth", "timeout", "owner"]
-        if all([req_field in data_dict.keys() for req_field in req_dict_fields]):
-            return True
-
-        else:
-            return False
+        """
+        Always returns True in current implementation.
+        """
+        return True
